11083b618961a6e82a693f798a8ea6d4e9457877
zhouxiaowen <00441208@want-want.com>
2023-11-02 14:34:18 +0800

    插值表达式

--- vue-demo/src/App.vue
+++ vue-demo/src/App.vue
@@ -1,13 +1,12 @@
 <script setup>
 import { ref } from 'vue';
 /* 节点中间用于放置 javascript 代码； */
-const count = ref(0)
+const title = ref('插值表达式')
 </script>
 
 <template>
  <!--  节点中间用于放置 html 代码； -->
- <h1>Hello vue3!</h1>
- <button @click="count++">count: {{count}}</button>
+ <h1>{{title}}</h1>
 </template>
 
 <style scoped>
