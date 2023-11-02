e2109c973dcce4f77c8578b1f7f792d65791387b
zhouxiaowen <00441208@want-want.com>
2023-11-02 16:38:03 +0800

    循环

--- vue-demo/src/App.vue
+++ vue-demo/src/App.vue
@@ -1,35 +1,20 @@
 <script setup>
-import { reactive, ref } from 'vue';
+import { ref } from 'vue';
 
-const obj = reactive({
-  text: '<h1>🫅</h1>'
-});
-
-const path = ref("https://www.quanxiaoha.com")
-
-const isActive = ref(true)
-
-const classes = {
-  red: 'red',
-  background : 'bg-back'
-}
-
-const isShow = ref(true)
-
-const type = ref(1)
-
+const tutorials = ref(['Vue 3 教程', 'IDEA 使用教程', 'Docker 教程'])
 </script>
 
 <template>
- <div>
-   <p><a v-bind:class="[classes.red,classes.background]">哈哈</a></p>
-   <p><a v-bind:class="isActive ? 'red':'' ">哈哈</a></p>
- </div>
-  <!-- 通过 v-if 指令判断 isShow 是否为 true, 若是则渲染 <a> 标签，否则不渲染 -->
-   <a href="https://www.quanxiaoha.com" v-if="isShow">犬小哈教程</a>
-   <a href="https://www.quanxiaoha.com" v-else-if="type === 2">犬小哈教程2</a>
-    <a href="https://www.quanxiaoha.com" v-else-if="type === 3">犬小哈教程3</a>
-   <div v-else>www.quanxiaoha.com</div>
+   <table border="1" width="600px">
+    <tr>
+      <td>序号</td>
+      <td>名称</td>
+    </tr>
+    <tr v-for="(v, i) in tutorials" :key="i">
+      <td>{{ i + 1 }}</td>
+      <td>{{ v }}</td>
+    </tr>
+  </table>
 </template>
 
 <style scoped>
