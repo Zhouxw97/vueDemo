20c339ff020cd034a93daf8f099c6da950b717d3
zhouxiaowen <00441208@want-want.com>
2023-11-02 11:33:19 +0800

    代码删除掉，只保留结构

--- vue-demo/src/App.vue
+++ vue-demo/src/App.vue
@@ -1,12 +1,12 @@
 <script setup>
-// 节点中间用于放置 javascript 代码；
+/* 节点中间用于放置 javascript 代码； */
 
 </script>
 
 <template>
- // 节点中间用于放置 html 代码；
+ <!--  节点中间用于放置 html 代码； -->
 </template>
 
 <style scoped>
-
+   /* 节点中间用于放置 css 代码； */
 </style>
