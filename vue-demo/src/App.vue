6195c906afe922ba21483c9bc6eab3ed4878e29c
zhouxiaowen <00441208@want-want.com>
2023-11-02 15:59:55 +0800

    v-bind

--- vue-demo/src/App.vue
+++ vue-demo/src/App.vue
@@ -1,15 +1,20 @@
 <script setup>
-import { reactive } from 'vue';
+import { reactive, ref } from 'vue';
 
 const obj = reactive({
   text: '<h1>🫅</h1>'
 });
+
+const path = ref("https://www.quanxiaoha.com")
 
 </script>
 
 <template>
  <div v-text="obj.text"></div>
  <div v-html="obj.text"></div>
+
+ <a :href="path" style="display: block;">小哈教程</a>
+ <a v-bind:href="path" style="display: block;">小哈教程</a>
 </template>
 
 <style scoped>
