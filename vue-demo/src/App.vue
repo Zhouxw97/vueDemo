40c7ab04cd94409e5760a7a88dcdebcee36ceb95
zhouxiaowen <00441208@want-want.com>
2023-11-02 14:28:46 +0800

    button按钮计数

--- vue-demo/src/App.vue
+++ vue-demo/src/App.vue
@@ -1,10 +1,13 @@
 <script setup>
+import { ref } from 'vue';
 /* 节点中间用于放置 javascript 代码； */
-
+const count = ref(0)
 </script>
 
 <template>
  <!--  节点中间用于放置 html 代码； -->
+ <h1>Hello vue3!</h1>
+ <button @click="count++">count: {{count}}</button>
 </template>
 
 <style scoped>
