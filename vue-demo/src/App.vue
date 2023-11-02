6f96062095f0938482e7557eeed79616d21a8b44
zhouxiaowen <00441208@want-want.com>
2023-11-02 15:05:16 +0800

    v-html

--- vue-demo/src/App.vue
+++ vue-demo/src/App.vue
@@ -1,38 +1,15 @@
 <script setup>
 import { reactive } from 'vue';
+
 const obj = reactive({
-  title: 'Hello World',
-  num:1,
-  num2:2,
-  isSucces:true,
-  arr:[1,2,3,4,5],
-  subObj:{
-    name:'subObj',
-    age:18
-  }
+  text: '<h1>🫅</h1>'
 });
 
 </script>
 
 <template>
- <div>{{ obj.title }}</div>
- <div>{{ obj.num }}</div>
- <div>{{ obj.num > obj.num2 }}</div>
-
- <div>{{ obj.isSucces }}</div>
-
- <div>{{ obj.arr }}</div>
-
- <div>{{ obj.subObj }}</div>
- <div>{{ obj.subObj.name }}</div>
-
- <div>{{ obj.isSucces? 1:2 }}</div>
- <div v-text="obj.title"></div>
-
- <div v-once>{{ obj.title }}</div>
- <input v-model="obj.title" type="text">
-
-
+ <div v-text="obj.text"></div>
+ <div v-html="obj.text"></div>
 </template>
 
 <style scoped>
